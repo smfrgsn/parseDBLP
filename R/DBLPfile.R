3094af17cbaea59ac98a8a9697d571947ae0b4fd
smfrgsn <samuel.john.ferguson@gmail.com>
2015-07-19 20:15:19 +1000

    Basic Structure

--- R/DBLPfile.R
+++ R/DBLPfile.R
@@ -2,15 +2,21 @@ DBLPfile <- function(filename){
   
   # The DBLP filename is found by either 
   
-  
   # Looking at the first argument or
+  if (nargs>0){
+    file = filename
+  } else if (file.exists("../../data/parseDBLP/dblp.xml")){
+  # Checking if it exists at ../../data/parseDBLP/
+    file = "../../data/parseDBLP/dblp.xml"
   
+  } else {
   # if it is not there
-  
-  # Downloadign the file
-  
-  # Saving it to a sensible place
-  
-  # using it as the argument  
-  
+  # Downloading the file
+  # Saving it to a sensible place  
+  println("please download the dblp file from:")
+  println("http://dblp.uni-trier.de/xml/ and save it ")
+  println("in the appropriate data directory")
+  file = "not set";
+  }
+
 }
--- R/parseDBLPXML.R
+++ R/parseDBLPXML.R
@@ -1,62 +1,67 @@
-require(XML)
-branchFunction <- function() {
-  store <- new.env() 
-    
-  func <- function(x, ...) {
-    ns <- getNodeSet(x, path = "//author")
-    authorArray = {};
-    for(i in 1:100){
-      value <- xmlValue(ns[[i]])
-      print(value)
-      authorArray[i] = value;
-    }
-    key = "author";
-    store[[key]] <- authorArray; 
-  }
-  getStore <- function() { as.list(store) }
-  list(dblp=func, getStore=getStore)
-}
-
-myfunctions <- branchFunction()
-
-xmlEventParse(
-  file = "~/Downloads/dblp3.xml", 
-  handlers = NULL, 
-  branches = myfunctions,
-  replaceEntities=TRUE
-)
-
-
-s = xmlTreeParse("~/Downloads/dblp3.xml")
-
-titles = list()
-xmlTreeParse("~/Downloads/dblp2.xml", replaceEntities=TRUE, handlers = list(author = function(x)
-  titles[[length(titles) + 1]] <<- x))
-sapply(titles, xmlValue)
-
-
-#to see what is inside
-myfunctions$getStore()
-
-
-
-
-dblpblah = function(x, ...) {
-  #This is a "branch" function
-  #x is a XML node - everything inside element 
-  # find element <ArticleTitle> inside and print it:
-  ns2 <- getNodeSet(x,path = "//title")
-  authorArray = {};
-  for (i in 1:length(ns2)){
-  value <- xmlValue(ns2[[i]])
-    print(value)
-   authorArray[i] = value;
-  }
+parseDBLPXML <- function(searchString, searchField){
+  require(XML)
+  dblpfile = DBLPfile();
+  titles = list()
   
+  save_title_function = function(x){
+    titles[[length(titles) + 1]] <<- x
+  }
+  xmlTreeParse(dblpfile, eval(quote(2 + 2))handlers = list(author = ))
+  sapply(titles, xmlValue)
 }
 
-xmlEventParse(
-  file = "~/Downloads/dblp3.xml", 
-  handlers = NULL, 
-  branches = list(dblp = dblpblah)
-)
+
+# branchFunction <- function() {
+#   store <- new.env() 
+#     
+#   func <- function(x, ...) {
+#     ns <- getNodeSet(x, path = "//author")
+#     authorArray = {};
+#     for(i in 1:100){
+#       value <- xmlValue(ns[[i]])
+#       print(value)
+#       authorArray[i] = value;
+#     }
+#     key = "author";
+#     store[[key]] <- authorArray; 
+#   }
+#   getStore <- function() { as.list(store) }
+#   list(dblp=func, getStore=getStore)
+# }
+# 
+# myfunctions <- branchFunction()
+# 
+# xmlEventParse(
+#   file = "~/Downloads/dblp3.xml", 
+#   handlers = NULL, 
+#   branches = myfunctions,
+#   replaceEntities=TRUE
+# )
+
+
+
+
+
+
+
+# 
+# 
+# dblpblah = function(x, ...) {
+#   #This is a "branch" function
+#   #x is a XML node - everything inside element 
+#   # find element <ArticleTitle> inside and print it:
+#   ns2 <- getNodeSet(x,path = "//title")
+#   authorArray = {};
+#   for (i in 1:length(ns2)){
+#   value <- xmlValue(ns2[[i]])
+#     print(value)
+#    authorArray[i] = value;
+#   }
+#   
+# }
+# 
+# xmlEventParse(
+#   file = "~/Downloads/dblp3.xml", 
+#   handlers = NULL, 
+#   branches = list(dblp = dblpblah)
+# )
